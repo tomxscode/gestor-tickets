c714f233b539a14c7645fa10ac4de49241bebc61
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-04-15 20:44:09 -0400

    Header

---
+++ clientes/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "../core/views/head.php"; ?>
+    <link rel="stylesheet" href="../core/views/bootstrap.min.css">
+    <title>Clientes</title>
+</head>
+<body>
+    <?php require_once "../core/views/header.php"; ?>
+</body>
+</html>
---
+++ usuario/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "../core/views/head.php"; ?>
+    <link rel="stylesheet" href="../core/views/bootstrap.min.css">
+    <title>Mi cuenta</title>
+</head>
+<body>
+    <?php require_once "../core/views/header.php"; ?>
+    
+    <div class="container">
+        <div class="row">
+            
+        </div>
+    </div>
+</body>
+</html>
