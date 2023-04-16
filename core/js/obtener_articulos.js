f6d94678680423c9f6fd22978c53a0cc14a3500b
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-04-15 21:18:58 -0400

    Edición.

--- core/js/obtener_articulos.js
+++ core/js/obtener_articulos.js
@@ -1,23 +1,26 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function () {
     obtenerClientes();
-})
+});
 
 function obtenerClientes() {
-    $.ajax({
-        url: "../core/clientes/obtener_clientes.php",
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "../core/clientes/obtener_clientes.php");
+    xhr.responseType = "json";
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var data = xhr.response;
             console.log(data);
-            // si se obtuvieron resultados
             for (var i = 0; i < data.length; i++) {
                 var cliente = data[i];
-                var html = "<tr><td>" + cliente.id + "</td><td>" + cliente.nombre + "</td><td>" + cliente.telefono + "</td></tr>"
-                $("#tabla-clientes").append(html);
+                var html = "<tr><td>" + cliente.id + "</td><td>" + cliente.nombre + "</td><td>" + cliente.telefono + "</td></tr>";
+                document.querySelector("#tabla-clientes").innerHTML += html;
             }
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            console.log("Error al obtener los archivos: " + textStatus);
+        } else {
+            console.log("Error al obtener los archivos: " + xhr.statusText);
         }
-    })
+    };
+    xhr.onerror = function () {
+        console.log("Error de red al obtener los archivos");
+    };
+    xhr.send();
 }
