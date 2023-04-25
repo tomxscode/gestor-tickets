be85ad088d94493efb480f7bdb583e90afa196ef
Tomás Barros <104880585+tomxscode@users.noreply.github.com>
2023-04-24 21:00:26 -0400

    Nueva columna

--- sql.sql
+++ sql.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 23, 2023 at 06:48 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-04-2023 a las 02:59:47
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `serv_tecnico`
+-- Base de datos: `serv_tecnico`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `clientes`
+-- Estructura de tabla para la tabla `clientes`
 --
 
 CREATE TABLE `clientes` (
@@ -34,7 +34,7 @@ CREATE TABLE `clientes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `clientes`
+-- Volcado de datos para la tabla `clientes`
 --
 
 INSERT INTO `clientes` (`id`, `nombre`, `telefono`) VALUES
@@ -46,7 +46,7 @@ INSERT INTO `clientes` (`id`, `nombre`, `telefono`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `equipos`
+-- Estructura de tabla para la tabla `equipos`
 --
 
 CREATE TABLE `equipos` (
@@ -59,7 +59,7 @@ CREATE TABLE `equipos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `equipos`
+-- Volcado de datos para la tabla `equipos`
 --
 
 INSERT INTO `equipos` (`id`, `modelo`, `marca`, `serial`, `dueno`, `comentarios`) VALUES
@@ -72,7 +72,7 @@ INSERT INTO `equipos` (`id`, `modelo`, `marca`, `serial`, `dueno`, `comentarios`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tr_acciones`
+-- Estructura de tabla para la tabla `tr_acciones`
 --
 
 CREATE TABLE `tr_acciones` (
@@ -87,7 +87,7 @@ CREATE TABLE `tr_acciones` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tr_equipo`
+-- Estructura de tabla para la tabla `tr_equipo`
 --
 
 CREATE TABLE `tr_equipo` (
@@ -99,7 +99,7 @@ CREATE TABLE `tr_equipo` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tr_informacion`
+-- Estructura de tabla para la tabla `tr_informacion`
 --
 
 CREATE TABLE `tr_informacion` (
@@ -108,13 +108,14 @@ CREATE TABLE `tr_informacion` (
   `ingreso` date NOT NULL,
   `egreso` date NOT NULL,
   `estado` int(11) NOT NULL,
-  `precio` int(11) NOT NULL
+  `precio` int(11) NOT NULL,
+  `diag_inicial` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -125,31 +126,31 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `email`, `contrasena`) VALUES
 (1, 'hola', 'tomas@admin.cl', '$2y$10$R5hR7/3B0tJdHxf9Glx8YukwOKzEVGzoqpcEQeCa53PkKWeLAdEeO');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `clientes`
+-- Indices de la tabla `clientes`
 --
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `equipos`
+-- Indices de la tabla `equipos`
 --
 ALTER TABLE `equipos`
   ADD PRIMARY KEY (`id`),
   ADD KEY `cliente_id` (`dueno`);
 
 --
--- Indexes for table `tr_acciones`
+-- Indices de la tabla `tr_acciones`
 --
 ALTER TABLE `tr_acciones`
   ADD PRIMARY KEY (`id`),
@@ -157,7 +158,7 @@ ALTER TABLE `tr_acciones`
   ADD KEY `accionador` (`accionador`);
 
 --
--- Indexes for table `tr_equipo`
+-- Indices de la tabla `tr_equipo`
 --
 ALTER TABLE `tr_equipo`
   ADD PRIMARY KEY (`id`),
@@ -165,84 +166,84 @@ ALTER TABLE `tr_equipo`
   ADD KEY `equipo_id` (`equipo_id`);
 
 --
--- Indexes for table `tr_informacion`
+-- Indices de la tabla `tr_informacion`
 --
 ALTER TABLE `tr_informacion`
   ADD PRIMARY KEY (`id`),
   ADD KEY `identificador_trabajo` (`identificador_trabajo`);
 
 --
--- Indexes for table `usuarios`
+-- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `clientes`
+-- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `equipos`
+-- AUTO_INCREMENT de la tabla `equipos`
 --
 ALTER TABLE `equipos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `tr_acciones`
+-- AUTO_INCREMENT de la tabla `tr_acciones`
 --
 ALTER TABLE `tr_acciones`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `tr_equipo`
+-- AUTO_INCREMENT de la tabla `tr_equipo`
 --
 ALTER TABLE `tr_equipo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `tr_informacion`
+-- AUTO_INCREMENT de la tabla `tr_informacion`
 --
 ALTER TABLE `tr_informacion`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `usuarios`
+-- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `equipos`
+-- Filtros para la tabla `equipos`
 --
 ALTER TABLE `equipos`
   ADD CONSTRAINT `equipos_ibfk_1` FOREIGN KEY (`dueno`) REFERENCES `clientes` (`id`);
 
 --
--- Constraints for table `tr_acciones`
+-- Filtros para la tabla `tr_acciones`
 --
 ALTER TABLE `tr_acciones`
   ADD CONSTRAINT `tr_acciones_ibfk_1` FOREIGN KEY (`equipo_identificador`) REFERENCES `tr_equipo` (`id`),
   ADD CONSTRAINT `tr_acciones_ibfk_2` FOREIGN KEY (`accionador`) REFERENCES `usuarios` (`id`);
 
 --
--- Constraints for table `tr_equipo`
+-- Filtros para la tabla `tr_equipo`
 --
 ALTER TABLE `tr_equipo`
   ADD CONSTRAINT `tr_equipo_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`id`),
   ADD CONSTRAINT `tr_equipo_ibfk_2` FOREIGN KEY (`equipo_id`) REFERENCES `equipos` (`id`);
 
 --
--- Constraints for table `tr_informacion`
+-- Filtros para la tabla `tr_informacion`
 --
 ALTER TABLE `tr_informacion`
   ADD CONSTRAINT `tr_informacion_ibfk_1` FOREIGN KEY (`identificador_trabajo`) REFERENCES `tr_equipo` (`id`);
